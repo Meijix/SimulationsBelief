b048d1fd666d09e1454167fb71d1a730c8068dd3
Philip-Sink <sinkphilip22@gmail.com>
2026-08-14 16:14:43 -0400

    Only confusion

--- examples3.py
+++ examples3.py
@@ -30,22 +30,10 @@ frame3 = {"a": {("w1","w2"),("w1","w3")},"b": {("w3","w2"),("w2","w3")},"c": {("
 
 RF3 = RelationalFrame.from_partial(agents, worlds, frame3, make_serial=True)
 
-print(f"{is_proper(RF3)}")
-
-print(f"{non_proper_worlds(RF3)}")
-
 preview(RF3, "Example 3")
-
-# Unless I'm missing something, the above should be in error. It spits ou all three worlds, but it should only spit out w2 and w3, no?
 
 #frame3.1 = {"a": s5_closure(worlds, frame3),"b": s5_closure(worlds, frame3),"c": s5_closure(worlds, frame3)}
 
 #RF3.1 = RelationalFrame(agents, worlds, frame3.1)
 
 #In the above two lines, I am having an issue with the s5_closure function - what's my issue?
-
-print(f"{is_proper(RF)}")
-
-print(f"{non_proper_worlds(RF)}")
-
-# Here might also be an error. RF is not S5. This is probably related to the issue above as well I suspect.
