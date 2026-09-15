111e17cb628c6261762a8e78bad418289246ab46
Philip-Sink <sinkphilip22@gmail.com>
2026-09-14 20:07:27 -0400

    Some jazz

---
+++ examples7.py
@@ -0,0 +1,33 @@
+import sys
+from knowledge_belief import KnowledgeBeliefFrame
+from visualization import preview, show, to_dot, visualize
+from relational_frame import RelationalFrame
+from simplicial import to_simplicial
+from properness import is_proper, non_proper_worlds, explain, to_proper
+from hasse import show
+
+agents = {"a", "b", "c"}
+worlds = {"w1", "w2", "w3"}
+frame = {"a":{("w1","w2"),("w1","w3")}, "b":{("w1","w2"),("w3","w2")}, "c":{("w2","w3"),("w1","w3")}}
+
+KBframe = KnowledgeBeliefFrame.from_partial(agents, worlds, frame, frame)
+preview(KBframe, "KB frame")
+
+print(f"{KnowledgeBeliefFrame.is_proper(KBframe)}")
+
+#print(f"{KnowledgeBeliefFrame.non_proper_worlds(BKframe)}")
+
+KBproperframe = KnowledgeBeliefFrame.to_proper(KBframe)
+preview(KBproperframe, "KB proper frame")
+
+print(f"{KnowledgeBeliefFrame.is_proper(KBproperframe)}")
+
+#It's not picking out the smallest frame here. Should be either b or c as the distinguished agent.
+
+#print(f"{non_proper_worlds(BKproperframe)}")
+
+Simp = to_simplicial(KBproperframe)
+
+show(Simp, "Simplicial Model")
+
+#Can't get it to visualize hasse diagrams no matter what I try.
---
+++ examples8.py
@@ -0,0 +1,9 @@
+import sys
+from knowledge_belief import KnowledgeBeliefFrame
+from visualization import preview, show, to_dot, visualize
+from relational_frame import RelationalFrame
+from simplicial import to_simplicial
+from properness import is_proper, non_proper_worlds, explain, to_proper
+
+
+
